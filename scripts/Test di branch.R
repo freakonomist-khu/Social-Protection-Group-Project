47369b2cf730b0a4af2f441ba28e83fb550232c7
freakonomist-khu <f.bappenas@gmail.com>
2026-05-17 21:17:57 +0900

    update branch the-first-branch new commit

--- scripts/Test di branch.R
+++ scripts/Test di branch.R
@@ -1,4 +1,13 @@
 library(tidyr)
+library(ggplot2)
 
 a<-1+1
 print(a)
+
+df <- read.csv("D:/0x. Master of Infrastructure Development Policy/03. Spring Semester 2026/Field Data Analysis/Group Project/Social-Protection-Group-Project/data/PER-Table1-Key-Indicators.csv")
+
+head(df)
+ggplot(df, aes(x=, y=Indicator.Value)) + 
+  geom_bar(stat="identity") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  labs(title="Indicator Value by Country", x="Country", y="Indicator Value")
