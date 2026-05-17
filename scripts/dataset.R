5f3635deb45866ba60d5b7654c6e5bbed787bdbd
freakonomist-khu <f.bappenas@gmail.com>
2026-05-17 14:15:46 +0900

    delete the unnecesarry setwd line

--- scripts/dataset.R
+++ scripts/dataset.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-df <- read.csv("D:/0x. Master of Infrastructure Development Policy/03. Spring Semester 2026/Field Data Analysis/Group Project/Social-Protection-Group-Project/data/PER-Table1-Key-Indicators.csv")
+df <- read.csv("data/PER-Table1-Key-Indicators.csv")
 summary(df)
 
-setwd("D:/0x. Master of Infrastructure Development Policy/03. Spring Semester 2026/Field Data Analysis/Group Project/Social-Protection-Group-Project/scripts")
+getwd()
